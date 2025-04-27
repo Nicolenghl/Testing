ca5920151b11b71c8407e7f9a01cf423eb4c00a2
Nicolenghl <amend.bascule-2z@icloud.com>
2025-04-28 05:20:52 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
+   import './globals.css';
+   import { Web3Provider } from './context/Web3Context';
+   
+   export default function RootLayout({ children }) {
+     return (
+       <html lang="en">
+         <body>
+           <Web3Provider>
+             {children}
+           </Web3Provider>
+         </body>
+       </html>
+     );
+   }
