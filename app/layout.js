0d2361fd59ecf9cd78d899fd8ff0675b8c6b5288
Nicolenghl <amend.bascule-2z@icloud.com>
2025-04-28 10:50:35 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
-   import './globals.css';
-   import { Web3Provider } from './context/Web3Context';
-   
-   export default function RootLayout({ children }) {
-     return (
-       <html lang="en">
-         <body>
-           <Web3Provider>
-             {children}
-           </Web3Provider>
-         </body>
-       </html>
-     );
-   }
+'use client';
+
+import './globals.css';
+import { Web3Provider } from './context/Web3Context';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.ethers.io/lib/ethers-5.7.umd.min.js" type="application/javascript"></script>
+      </head>
+      <body>
+        <Web3Provider>
+          {children}
+        </Web3Provider>
+      </body>
+    </html>
+  );
+}
